bc3ce5d5c7910fd7ff579f47dacf22b30252d8e3
David Garcia <286995159+Code-D-Garcia@users.noreply.github.com>
2026-06-12 11:50:28 -0500

    feat: add experience section

---
+++ src/data/experience.ts
@@ -0,0 +1,14 @@
+export const experienceData = [
+	{
+		id: 'blueminds',
+		stack: ["Odoo", "Python", "JavaScript", "XML", "PostgreSQL", "Docker"]
+	},
+	{
+		id: 'epam_iot',
+		stack: ["IoT", "Node JS", "Express JS", "MongoDB", "Arduino", "ESP32", "DHT22 Sensor"]
+	},
+	{
+		id: 'epam_backend',
+		stack: ["Express", "TypeScript", "React", "Bootstrap", "PostgreSQL"]
+	}
+];

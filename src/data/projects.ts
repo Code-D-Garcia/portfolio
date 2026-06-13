ed1d99a286679f5ddecfb07725c9bf99277e3b44
David Garcia <286995159+Code-D-Garcia@users.noreply.github.com>
2026-06-13 17:04:27 -0500

    feat: add projects images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,11 @@
+import chiroWeb from '../assets/captures/chiro_web.webp';
+
 export const projectsData = [
 	{
 		id: 'chiro',
 		stack: ["Spring Boot", "PostgreSQL", "Spring Security", "Hibernate/Jpa", "Auth/JWT", "React"],
 		link: "#",
-		repo: "https://github.com/Code-D-Garcia/personal-expenses-backend"
+		repo: "https://github.com/Code-D-Garcia/personal-expenses-backend",
+		image: chiroWeb
 	}
 ];

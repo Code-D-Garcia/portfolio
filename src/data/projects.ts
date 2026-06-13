7eb9afea4732b6399725493a333cc23908a4fb9c
David Garcia <286995159+Code-D-Garcia@users.noreply.github.com>
2026-06-13 17:51:41 -0500

    chore: add demo links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,9 +4,9 @@ export const projectsData = [
 	{
 		id: 'chiro',
 		stack: ["Spring Boot", "PostgreSQL", "Spring Security", "Hibernate/Jpa", "Auth/JWT", "React"],
-		link: "#",
+		link: "https://youtu.be/3fWum26r_Cc",
 		repo: "https://github.com/Code-D-Garcia/personal-expenses-backend",
 		image: chiroWeb,
-		appLink: "#"
+		appLink: "https://youtube.com/shorts/L0jcoHum2SA"
 	}
 ];
